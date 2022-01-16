f318c6300fce9c8596a62caaae97e7c1c93f38cd
nikhil yadav <iamnikhilyadav27@gmail.com>
2022-01-16 19:51:56 +0530

    created the job schema

---
+++ src/database/schemas/jobSchema.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const jobSchema = new Schema({
+  language: {
+    type: String,
+    required: true,
+    enum: ["cpp", "py", "js"],
+  },
+  filePath: {
+    type: String,
+    required: true,
+  },
+  submittedAt: {
+    type: Date,
+    default: Date.now,
+  },
+  startedAt: {
+    type: Date,
+  },
+  completedAt: {
+    type: Date,
+  },
+  output: {
+    type: String,
+  },
+  status: {
+    type: String,
+    default: "pending",
+    enum: ["pending", "success", "error"],
+  },
+});
+
+module.exports = jobSchema;
